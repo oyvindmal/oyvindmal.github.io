82e4da658159484eb60f90459dcb958c5fbe7e84
Øyvind Malin <oyvindmalin@oyvindmal.onmicrosoft.com>
2018-05-21 13:46:27 +0200

    added updating and arrow

--- liverating.js
+++ liverating.js
@@ -1,27 +1,47 @@
+var apibase = "https://lichess.org/api";
+
 $(document).ready(function () {
 	// Api
-  var apibase = "https://lichess.org/api";
-  
+ 
   
   
   //config
   var mode = "blitz";
   var username = "oyvindmal";
-  
-  
-  
-  var apiurl = apibase + "/user/" + username;
-  
- 	$.getJSON(apiurl, function(data)
-  {
-  	var elo = data.perfs.blitz.rating;
-    var prog = data.perfs.blitz.prog;
-      
-	$("#currentElo").text(elo);
 
-  $("#currentProgr").text(prog);
+  setInterval(getLiveRating(username), 30000);
   
-  })
-
+ 
     $("#currentMode").text(mode);
 })
+
+function getLiveRating(user)
+{
+    var apiurl = apibase + "/user/" + user;
+    
+    console.log("Getting data");
+       $.getJSON(apiurl, function(data)
+    {
+        var elo = data.perfs.blitz.rating;
+      var prog = data.perfs.blitz.prog;
+
+        
+      $("#currentElo").text(elo);
+        var icon = $("#icon");
+    $("#currentProgr").text(prog);
+    if(prog === 0)
+        {
+            icon.attr('class', 'iconEqual');
+        }
+
+        if(prog > 0)
+            {
+                icon.attr('class', 'iconPositive');
+            }
+            if(prog < 0)
+                {
+                    icon.attr('class', 'iconNegative');
+                }
+    })
+  
+}
